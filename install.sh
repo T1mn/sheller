f1837c11726a6d1fe955a35ccc844c9b5c019125
Tang Guofang <tangguofang@saicmotor.com>
2022-10-15 18:21:09 +0800

    feature: update install.sh
    delete all shell tools in shellrcs when exec install.sh, then echo these shell tools paths to shellrcs.

--- install.sh
+++ install.sh
@@ -4,7 +4,17 @@
 
 # vars
 path=$(dirname $(readlink -f "$0"))
-target_path="/etc"
+target_path="/home/${USER}/.shell_tools"
+
+# init directory
+function init () {
+    if [ -d "/home/${USER}/.shell_tools" ]; then
+        echo "directory \"/home/${USER}/.shell_tools\" exists"
+    else
+        echo "create directory \"/home/${USER}/.shell_tools\""
+        $(mkdir -p /home/${USER}/.shell_tools)
+    fi
+}
 
 # find configs(shrc)
 function get_base_shell_config () {
@@ -17,19 +27,24 @@ function update_shell_config () {
     do
         # echo "grep -c \""$1"\" "$shell_config""
         if [ $(grep -c "$1" $shell_config) -ne '0' ];then
-            echo "skip "$1">>"$shell_config" ..."
+            echo "update "$1">>"$shell_config" ..."
+            # echo "$2"
+            filename_=$(echo "$2" | sed 's/\./\\&/g' | sed 's/\//\\&/g')
+            # echo "sed -i "/"$filename_"/d" "$shell_config""
+            sed -i "/"$filename_"/d" "$shell_config"
         else
             echo "start "$1">>"$shell_config" ..."
-            echo "$1">>"$shell_config"
         fi
+        echo "$1">>"$shell_config"
     done
 }
 
 # install
 function install_shell_tools () {
+    init
     get_base_shell_config
     echo 'start installing...'
-    update_shell_config "export SHELL_PATH=${path}" # update env
+    update_shell_config "export SHELL_PATH=${path}" "${path}" # update env
     files=$(ls $path | grep ".sh$" | grep -v "install")
     for filename in $files
     do
@@ -37,7 +52,7 @@ function install_shell_tools () {
         target_file="$target_path"/"$filename"
         $(sudo rm -rf "${target_file}")
         $(sudo ln -s "${source_file}" "${target_file}")
-        update_shell_config "source ${target_file}"
+        update_shell_config "source ${target_file}" "$filename"
     done
     echo 'install complete'
 }
